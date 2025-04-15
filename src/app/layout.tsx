b249312f6aa99cdcce98874af467c87edcffd487
snguyen56 <101228542+snguyen56@users.noreply.github.com>
2025-04-15 14:21:41 -0400

    removed initial content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Open_Sans } from "next/font/google";
+import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const openSans = Open_Sans({
   variable: "--font-open-sans",
@@ -31,11 +21,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Header />
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${openSans.variable} font-[family-name:var(--font-open-sans)] antialiased container mx-auto text-zinc-500`}
+        className={`${openSans.variable} font-[family-name:var(--font-open-sans)] antialiased container mx-auto text-zinc-500`}
       >
-        {children}
+        <Header />
+        <div className="mt-24 mb-16">{children}</div>
         <Footer />
       </body>
     </html>
